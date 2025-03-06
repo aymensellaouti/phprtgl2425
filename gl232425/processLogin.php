e2d07eddf278553ffb4d78fdcccd8e715e0f9dd2
aymensellaouti <aymen.sellaouti@gmail.com>
2025-03-06 11:45:16 +0100

    preparation de la seance du 6 3 2025

--- gl232425/processLogin.php
+++ gl232425/processLogin.php
@@ -1,21 +1,22 @@
-<?php 
+<?php
+session_start();
+// var_dump($_GET);
+// var_dump($_POST);
+// var_dump($_FILES); 
 
-    // var_dump($_GET);
-    // var_dump($_POST);
-    // var_dump($_FILES); 
+// if (isset($_FILES['fichier'])) {
+//     // as.jpg => uploads/12345667as.jpg
+//     $newFilePath = "uploads/".uniqid().$_FILES['fichier']['name'];
+//     move_uploaded_file($_FILES['fichier']['tmp_name'], $newFilePath);
+// }
+$email = strip_tags($_POST['email']);
+$password = strip_tags($_POST['password']);
 
-    // if (isset($_FILES['fichier'])) {
-    //     // as.jpg => uploads/12345667as.jpg
-    //     $newFilePath = "uploads/".uniqid().$_FILES['fichier']['name'];
-    //     move_uploaded_file($_FILES['fichier']['tmp_name'], $newFilePath);
-    // }
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    if (isset($email) && isset($password)) {
-        if ($email == "admin@gmail.com" && $password == "0000") {
-            header("Location:home.php");
-        } else {
-            header("Location:login.php?errorMessage=Veuillez vérifier vos crédentials");
-        }
+if (isset($email) && isset($password)) {
+    if ($email == "admin@gmail.com" && $password == "0000") {
+        $_SESSION['user'] = $email;
+        header("Location:home.php");
+    } else {
+        header("Location:login.php?errorMessage=Veuillez vérifier vos crédentials");
     }
+}
